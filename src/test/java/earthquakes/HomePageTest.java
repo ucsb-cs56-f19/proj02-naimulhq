0695f5412e3edfab5f3626419ea3a0157d6f4267
Naimul <naimul@ucsb.edu>
2019-11-26 17:45:54 -0800

    Created a Locations link which routes to correct page. Created controllers that handles locations searches and results

--- src/test/java/earthquakes/HomePageTest.java
+++ src/test/java/earthquakes/HomePageTest.java
@@ -76,8 +76,8 @@ public class HomePageTest {
     public void getHomePage_hasCorrectFirstPage() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.TEXT_HTML))
                 .andExpect(status().isOk())
-                .andExpect(xpath("/html/body/div/nav/div/ul[1]/li[2]/a").exists())
-                .andExpect(xpath("/html/body/div/nav/div/ul[1]/li[2]/a").string("Earthquake Search"));
+                .andExpect(xpath("/html/body/div/nav/div/ul[1]/li[1]/a").exists())
+                .andExpect(xpath("/html/body/div/nav/div/ul[1]/li[1]/a").string("Earthquake Search"));
     }
 
     @Test
@@ -88,5 +88,12 @@ public class HomePageTest {
                 .andExpect(xpath("/html/body/div/nav/div/ul[1]/li[3]/a").string("Users"));
     }
 
+    @Test
+    public void getHomePage_hasLocationsLink() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.TEXT_HTML))
+             .andExpect(status().isOk())
+             .andExpect(xpath("/html/body/div/nav/div/ul[1]/li[2]/a").exists())
+             .andExpect(xpath("/html/body/div/nav/div/ul[1]/li[2]/a").string("Locations"));
+ }
 
 }
