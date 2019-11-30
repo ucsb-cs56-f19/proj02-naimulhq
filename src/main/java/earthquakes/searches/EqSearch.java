e57f545506f1b419e2edd3269843ca9d777558ac
Naimul Hoque <naimul@ucsb.edu>
2019-11-30 10:26:28 -0800

    nh - Add lat,lon,location to EqSearch. Changed search.html to allow user input and changed result.html to show the three inputs in the view

--- src/main/java/earthquakes/searches/EqSearch.java
+++ src/main/java/earthquakes/searches/EqSearch.java
@@ -4,6 +4,11 @@ public class EqSearch{
     
     private int distance;
     private int minmag;
+    private long lat;
+    private long lon;
+    private String location;
+
+
 
     EqSearch(){};
     public int getDistance(){
@@ -14,6 +19,31 @@ public class EqSearch{
 	return minmag;
     }
 
+    public long getLon(){
+        return lon;
+    }
+
+    public long getLat(){
+        return lat;
+    }
+
+    public String getLocation(){
+        return location;
+
+    }
+
+    public void setLon(long lon){
+        this.lon = lon;
+    }
+
+    public void setLat(long lat){
+        this.lat = lat;
+    }
+
+    public void setLocation(String location){
+        this.location = location;
+        
+    }
     public void setMinmag(int minmag){
 	this.minmag = minmag;
     }
